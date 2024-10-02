d2b8bb47fb344195aff6105726ac47f30901fc09
Jack Aden <jeaden38@gmail.com>
2024-10-02 16:04:07 -0600

    "Initial commit"

---
+++ backend/src/main/java/com/medicalrecords/MedicalRecordManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.medicalrecords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalRecordManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalRecordManagementSystemApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/medicalrecords/MedicalRecordManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicalrecords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalRecordManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
